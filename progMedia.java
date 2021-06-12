d5ac2395cb040f60adbc103316ae7ee7d01d7bcf
rogeriosantos <rogerio.tuba@gmail.com>
2021-06-11 21:58:43 -0300

    Inclusao Projeto Media

---
+++ progMedia.java
@@ -0,0 +1,46 @@
+package aula2;
+
+import java.util.Scanner;
+
+public class CalculoMedia {
+
+	public static void main(String[] args) {
+		
+		//Modo 1
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite a nota 1:  ");
+		Double nota1 = scan.nextDouble();
+		System.out.println("A Nota 1 e: "+ nota1);
+		
+		
+		System.out.println("Digite a nota 2:  ");
+		Double nota2 = scan.nextDouble();
+		System.out.println("A Nota 2 e: "+ nota2);
+		
+		
+		System.out.println("Digite a nota 3:  ");
+		Double nota3 = scan.nextDouble();
+		System.out.println("A Nota 3 e: "+ nota3);
+		
+	
+		System.out.println("Digite a nota 4:  ");
+		Double nota4 = scan.nextDouble();
+		System.out.println("A Nota 4 e: "+ nota4);
+		
+		Double media = (nota1+nota2+nota3+nota4)/4;
+				
+		System.out.println("A media das notas sao : "+ media);*/
+		
+		
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+}
